cc9787e38c3d40b9642b685d1d80b14d4981941e
ANAS-AAMMARI <anasaammari5@gmail.com>
2025-08-08 10:25:56 +0100

    fix token path error in provision worker file

--- p1/scripts/provision-worker.sh
+++ p1/scripts/provision-worker.sh
@@ -8,12 +8,12 @@ dnf install -y curl
 
 # Attendre que le token du serveur soit disponible dans le dossier partagé
 echo "Attente du token du serveur..."
-while [ ! -f /vagrant/node-token ]; do
+while [ ! -f /vagrant/k3s/node-token ]; do
     sleep 10
 done
 
 # Lire le token et définir l'URL du serveur
-K3S_TOKEN=$(cat /vagrant/node-token)
+K3S_TOKEN=$(cat /vagrant/k3s/node-token)
 K3S_URL="https://192.168.56.110:6443"
 
 # Installer l'agent K3s avec les variables d’environnement nécessaires
